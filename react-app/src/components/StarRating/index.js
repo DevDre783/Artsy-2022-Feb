890eaade6dd49193184e03e72e415124391957aa
Andres Soca <lumen.development01@gmail.com>
2022-04-13 13:19:26 -0400

    added star ratings on the frontend

---
+++ react-app/src/components/StarRating/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { FaStar } from 'react-icons/fa'
+import "./StarRating.css"
+
+const StarRating = () => {
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+
+    return (
+        <div className='rating_container'>
+            {[...Array(5)].map((star, i) => {
+                const ratingValue = i + 1;
+
+                return (
+                    <label key={i}>
+                        <input
+                            type='radio'
+                            name="rating"
+                            value={ratingValue}
+                            onClick={() => setRating(ratingValue)}
+                        />
+                        <FaStar className='star' color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"} size={35}
+                            onMouseEnter={() => setHover(ratingValue)}
+                            onMouseLeave={() => setHover(null)}
+                        />
+                    </label>
+                )
+            })}
+        </div>
+    )
+}
+
+export default StarRating;
